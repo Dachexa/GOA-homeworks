2558f62b4819db35b65abfb5f84a6c276da0f1b2
Dachexa <dachizirakashvili1@gmail.com>
2024-05-17 15:47:04 +0400

    first commit

---
+++ level 01/homework01.py
@@ -0,0 +1,9 @@
+name = "me var dachi"
+hello = "gamarjoba"
+how_old_is_he = 13
+he_height = "chemi simaglea"
+my_team_leader = "chemi razmis lideria dachi"
+height = 1.60
+age = "chemi wlovanebaa"
+
+print(hello + " " + name + " " + he_height + " " + str(height) + " " + age + " " + str(how_old_is_he) + " " + my_team_leader)
