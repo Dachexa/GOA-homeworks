e2f21bc751fb443aa40d44f5f0e1b500429d1a6e
Dachexa <dachizirakashvili1@gmail.com>
2025-03-19 20:30:52 +0400

    update commit

---
+++ level 47/class/script.js
@@ -0,0 +1,35 @@
+// Data types
+// 1. integer
+// 2. String
+// 3. Float
+// 4. Boolean
+
+
+
+
+
+// ინიციალიზაცია / initialization
+
+let name = 'Dachi';
+
+// დეკლარაცია / declaration
+
+let age
+age = 25;
+
+console.log(age)
+
+
+
+
+let x = 10, y = 20, z = 30;
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+
+
+let x = 20;
+
+console.log(x + 30)
---
+++ level 47/home/script.js
@@ -0,0 +1,19 @@
+console.log("Enter your name: ")
+let name = "Dachi"
+
+console.log("Enter your last name: ")
+let lastName = "Zirakashvili"
+
+console.log("Enter your age: ")
+let age = 14
+
+console.log("Enter your location: ")
+let location = "Kachreti"
+
+console.log("Enter your mothers name: ")
+let mothersName = "Xatuna"
+
+console.log('Enter your mom last name: ')
+let momsLastName = "Shioshvili"
+
+
