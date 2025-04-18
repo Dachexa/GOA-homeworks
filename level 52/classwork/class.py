7e95f020cbb208f51bb71cbd69c8034eebf8d0ef
Dachexa <dachizirakashvili1@gmail.com>
2025-04-18 14:08:17 +0400

    update commit

---
+++ level 52/classwork/class.py
@@ -0,0 +1,13 @@
+def open_or_senior(data):
+    res = []
+    
+    for i in data:
+        age = i[0]
+        handicap = i[1]
+        
+        if age <= 55 and handicap > 7:
+            res.append("Senior")
+        else:
+            res.append('Open')
+        return res
+    
